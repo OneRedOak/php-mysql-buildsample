176932c27b6593a6fa6a71284ee0c763847de99d
Patrick <oneredoak@gmail.com>
2014-05-13 14:05:19 -0700

    Correction to PHP Syntax

--- tests/phpApp_test.php
+++ tests/phpApp_test.php
@@ -4,7 +4,7 @@ class PhpAppTest extends PHPUnit_Framework_TestCase{
   
   public function testFindData(){
       $phpApp = new PhpApp();
-      $result = findData(); // Returns data in column
+      $result = $phpApp->findData(); // Returns data in column
       $this->assertEquals('Tom B. Erichsen', $result);
   }
 }
