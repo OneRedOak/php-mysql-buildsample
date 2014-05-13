33726a2979269cc70fe4cece7f66a39b69672dba
Patrick <oneredoak@gmail.com>
2014-05-13 14:02:44 -0700

    Refactoring of Functions

--- phpApp.php
+++ phpApp.php
@@ -2,28 +2,21 @@
 
 class PhpApp {
 
-	public function connectToDb() {
+	public function findData() {
 		$link = mysql_connect('localhost', 'root', '');
 		/* If database exists, drop it */
 		if ($link) {
 			mysql_query('DROP DATABASE my_db', $link);
 		}
-		/* Create new databse */
-		echo("creating database!\n");
 	    mysql_query("CREATE DATABASE testPhpDb");
 	    mysql_select_db('testPhpDb');
-	    return $link;
-	}
 
-	public function setData($link) {
 		/* Creates table, populates with sample data */
 		$createTableQuery = "CREATE TABLE Customer;";
 		$setDataQuery = "INSERT INTO Customer (CustomerName, ContactName, Address) VALUES ('Cardinal','Tom B. Erichsen','Skagen 21');";
 		mysql_query($createTableQuery, $link);
 		mysql_query($setDataQuery, $link);
-	}
 
-	public function findData($link) {
 		/* Returns ContactName data */
 		return mysql_query("SELECT ContactName FROM testPhpDb;", $link);
 	}
--- tests/phpApp_test.php
+++ tests/phpApp_test.php
@@ -1,12 +1,10 @@
 <?php
-require_once('../phpApp.php');
+require_once('phpApp.php');
 class PhpAppTest extends PHPUnit_Framework_TestCase{
   
   public function testFindData(){
       $phpApp = new PhpApp();
-      $dbLink = connectToDb(); // Connects to DB, creating new one if needed
-      setData($dbLink); // Creates table, sets columns & data
-      $result = findData($dbLink); // Returns data in column
+      $result = findData(); // Returns data in column
       $this->assertEquals('Tom B. Erichsen', $result);
   }
 }
